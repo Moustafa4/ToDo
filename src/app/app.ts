4869f0d69ccbc3fc0c0d4bdb52e05c53424aa9c5
Moustafa Mahmoud <moustafamahmoud5600@gmail.com>
2026-02-17 13:13:41 +0200

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,66 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BsDatepickerDirective, BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { enGbLocale } from 'ngx-bootstrap/locale';
+import { BsLocaleService } from 'ngx-bootstrap/datepicker';
+
+defineLocale('en-gb', enGbLocale);
 
 @Component({
   selector: 'app-root',
-  standalone: true, // مهم جدًا
-  imports: [BsDatepickerModule, CommonModule],
+  standalone: true,
+  imports: [BsDatepickerModule, CommonModule, FormsModule],
   templateUrl: './app.html',
-  styleUrls: ['./app.css'], // جمع مش مفرد
+  styleUrls: ['./app.css'],
 })
 export class App {
-  @ViewChild(BsDatepickerDirective, { static: false }) datepicker?: BsDatepickerDirective;
+  bsInlineValue: Date = new Date();
+  editingIndex: number | null = null;
+  isedit = false;
 
-  @HostListener('window:scroll')
-  onScrollEvent() {
-    this.datepicker?.hide();
+  bsConfig = {
+    firstDayOfWeek: 0,
+  };
+  
+
+  constructor(private localeservice: BsLocaleService) {
+    this.localeservice.use('en-gb');
+  }
+
+  tasks: { title: string; detail: string; date: string }[] = [];
+  newtask = '';
+  newdes = '';
+
+  add() {
+    if (this.newtask.trim() && this.newtask.length >= 3) {
+      const taskData = {
+        title: this.newtask,
+        detail: this.newdes,
+        date: this.bsInlineValue ? this.bsInlineValue.toISOString().split('T')[0] : '',
+      };
+
+      if (this.editingIndex !== null) {
+        this.tasks[this.editingIndex] = taskData;
+        this.editingIndex = null;
+      } else {
+        this.tasks.push(taskData);
+      }
+      this.newtask = '';
+      this.newdes = '';
+      this.bsInlineValue = new Date();
+    }
+  }
+  remove(index: number) {
+    this.tasks.splice(index, 1);
+  }
+  edit(index: number, taskedit: { title: string; detail: string; date: string }) {
+      this.newtask = taskedit.title;
+      this.newdes = taskedit.detail;
+      this.bsInlineValue = new Date(taskedit.date);
+      this.editingIndex = index;
+      this.isedit = true;
+   
   }
 }
